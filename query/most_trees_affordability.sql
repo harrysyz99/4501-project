7b819f597c435712cb6586c486b8a0e140da05bb
tl3259 <tl3259@columbia.edu>
2023-12-10 14:08:27 -0500

    done assertion

--- query/most_trees_affordability.sql
+++ query/most_trees_affordability.sql
@@ -1,28 +1,14 @@
 
-(select zillow."RegionName" , avg(zillow.rent) as avg_price , count(tree_id) as tree_numebr
-, count(t311."Unique Key") as compaint_number
-from zillow
-inner join stc on zillow."RegionName" = stc.zipcode
-inner join t311 on t311."Incident Zip" = zillow."RegionName"
-where zillow.date >= '2023-01-01'
-and zillow.date < '2023-02-01'
-and t311."Created Date">= '2023-01-01'
-and t311."Created Date"< '2023-02-01'
-and stc.created_at< '2023-02-01'
+select zillow."RegionName" , TO_CHAR(AVG(zillow.rent), 'FM999,999,990.00') as avg_price
+from zillow inner join (
+SELECT zipcode, COUNT(*) AS total_trees
+FROM stc
+GROUP BY zipcode
+ORDER BY total_trees DESC
+LIMIT 10
+) t on t.zipcode = zillow."RegionName"
+where zillow.date >= '2023-08-01'
+and zillow.date < '2023-09-01'
 group by zillow."RegionName"
-HAVING avg(zillow.rent) is not NULL
-order by avg_price desc limit 5)
-union
-(select zillow."RegionName" , avg(zillow.rent) as avg_price , count(tree_id) as tree_numebr
-, count(t311."Unique Key") as compaint_number
-from zillow
-inner join stc on zillow."RegionName" = stc.zipcode
-inner join t311 on t311."Incident Zip" = zillow."RegionName"
-where zillow.date >= '2023-01-01'
-and zillow.date < '2023-02-01'
-and t311."Created Date">= '2023-01-01'
-and t311."Created Date"< '2023-02-01'
-and stc.created_at< '2023-02-01'
-group by zillow."RegionName"
-HAVING avg(zillow.rent) is not NULL
-order by avg_price ASC limit 5)
+order by avg_price desc;
+
