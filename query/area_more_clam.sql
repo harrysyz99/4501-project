ce65ecfc1521c411d51ce2b25cac32f7093a51da
Shiyang Zhang <80500324+harrysyz99@users.noreply.github.com>
2023-12-10 15:25:26 -0500

    finish

--- query/area_more_clam.sql
+++ query/area_more_clam.sql
@@ -3,4 +3,5 @@ SELECT "Incident Zip", COUNT(*) AS number_of_complaints
 FROM t311
 WHERE "Created Date" BETWEEN '2022-10-01' AND '2023-09-30'
 GROUP BY "Incident Zip"
-ORDER BY number_of_complaints DESC;
+ORDER BY number_of_complaints DESC
+LIMIT 10;
